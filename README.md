20a8253f1459e2fd4d50738fd407e1d8b42c97a8
Chris000600 <christopherwilliam00@yahoo.com>
2023-12-18 11:48:57 +0700

    skaffold setup

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Microservices-101---Blog
 
+on each folder:
+
 ```
 npm install
 npm start
+```
+
+on root folder:
+
+```
+skaffold dev
 ```
--- posts/index.js
+++ posts/index.js
@@ -45,6 +45,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('v55');
+  console.log('v10');
   console.log('Listening on 4000');
 });
